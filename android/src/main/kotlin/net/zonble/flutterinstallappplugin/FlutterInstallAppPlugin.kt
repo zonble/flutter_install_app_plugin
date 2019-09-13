640210b6bdc09de21705dfa0974ecb279f653be5
Sebastian Roth <sebastian@nextbase.co.uk>
2019-09-13 15:24:59 +0100

    Update AGP/Kotlin

--- android/src/main/kotlin/net/zonble/flutterinstallappplugin/FlutterInstallAppPlugin.kt
+++ android/src/main/kotlin/net/zonble/flutterinstallappplugin/FlutterInstallAppPlugin.kt
@@ -3,22 +3,22 @@ package net.zonble.flutterinstallappplugin
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class FlutterInstallAppPlugin(private val activity: Activity) : MethodCallHandler {
     companion object {
         @JvmStatic
-        fun registerWith(registrar: Registrar): Unit {
+        fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "flutter_install_app_plugin")
             channel.setMethodCallHandler(FlutterInstallAppPlugin(registrar.activity()))
         }
     }
 
-    override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "installApp" -> {
                 val args = call.arguments as? ArrayList<*>
