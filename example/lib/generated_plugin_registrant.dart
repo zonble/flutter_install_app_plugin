c706cc0130685c21205308b11db98e830a578e3f
Sebastian Roth <sebastian@nextbase.co.uk>
2021-09-09 10:43:07 +0100

    migrate android tooling

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -2,6 +2,8 @@
 // Generated file. Do not edit.
 //
 
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter_install_app_plugin/flutter_install_app_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
